52305d4675cc833280b743a03b089219f21ec42f
KoSS <Kossmos@i.ua>
2019-08-22 11:53:09 +0300

    newGrid

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return(
-        <dialogs>Dialogs</dialogs>
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Kostya
+                </div>
+                <div className={style.dialog}>
+                    Dima
+                </div>
+                <div className={style.dialog}>
+                    Igor
+                </div>
+                <div className={style.dialog}>
+                    Sergey
+                </div>
+                <div className={style.dialog}>
+                    Pasha
+                </div>
+                <div className={style.dialog}>
+                    Jenya
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.dialog}>hi how are u?</div>
+                <div className={style.dialog}>i good, are u?</div>
+                <div className={style.dialog}>me 2</div>
+            </div>
+        </div>
     )
 }
 
